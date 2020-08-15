0c7d273019519bf3543b20d6fa5c4e83b1f031cc
Matheus Henrique Julidori <matheus_julidori@yahoo.com.br>
2020-08-15 16:01:27 -0300

    staticfiles collect

--- relpi_mi_back/settings.py
+++ relpi_mi_back/settings.py
@@ -145,7 +145,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'accounts/static/'),
     os.path.join(BASE_DIR, 'relpi_miAPP/static/'),
 ]
 
