2ced8f878770b5fce380b5ad8e204931bf9a8d4d
Matheus Henrique Julidori <matheus_julidori@yahoo.com.br>
2020-08-27 22:12:36 -0300

    Migrations pedidos

--- relpi_miAPP/models.py
+++ relpi_miAPP/models.py
@@ -3,11 +3,16 @@ from accounts.models import Client,Helper
 from django.contrib import admin
 
 class Pedidos(models.Model):
-    email_client = models.ForeignKey(Client,on_delete=models.CASCADE) #User client
-    is_taken = models.BooleanField(verbose_name='is_taken')
-    email_helper = models.ForeignKey(Helper,on_delete=models.CASCADE) #User helper
+    email_client = models.CharField(max_length = 254)
+    client_name = models.CharField(max_length = 254)
+    client_phone = models.CharField(max_length = 254)
+    is_taken = models.BooleanField(verbose_name='is_taken',default = False)
+    email_helper = models.CharField(max_length = 254, default = 'a')
+    helper_name = models.CharField(max_length = 254, blank = False, default = 'a')
+    helper_phone = models.CharField(max_length = 254, blank = False, default = 'a')
+    task_name = models.CharField(max_length = 254, blank = False)
     description = models.TextField(verbose_name='description')
     payment = models.CharField(verbose_name='payment', max_length=1)
-    pago = models.BooleanField(verbose_name='pago')
+    pago = models.BooleanField(verbose_name='pago',default = False)
 
 admin.site.register(Pedidos)
