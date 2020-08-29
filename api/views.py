13e839aa9adc88d13cf256975ab8f300cb800a3a
Matheus Henrique Julidori <matheus_julidori@yahoo.com.br>
2020-08-29 12:05:04 -0300

    try,except

--- api/views.py
+++ api/views.py
@@ -12,27 +12,32 @@ from relpi_miAPP.models import Pedidos
 
 class CancelListView(APIView):
     def post (self, request):
-        id = request.data.get('id', None)
-        email_helper = request.data.get('email_client', None)
-        email = Pedidos.objects.filter(id=id).values('email_client')
-        if email == email_helper:
-            Pedidos.objects.filter(id=id).update(cancelado = True)
-            return Response(status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+        try:
+            id = request.data.get('id', None)
+            email_helper = request.data.get('email_client', None)
+            email = Pedidos.objects.filter(id=id).values('email_client')
+            if email == email_helper:
+                Pedidos.objects.filter(id=id).update(cancelado = True)
+                return Response(status=status.HTTP_200_OK)
+            else:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class FinishListView(APIView):
     def post (self, request):
-        id = request.data.get('id', None)
-        email_helper = request.data.get('email_helper', None)
-        email = Pedidos.objects.filter(id=id).values('email_helper')
-        if email == email_helper:
-            Pedidos.objects.filter(id=id).update(terminado = True)
-            return Response(status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
+        try:
+            id = request.data.get('id', None)
+            email_helper = request.data.get('email_helper', None)
+            email = Pedidos.objects.filter(id=id).values('email_helper')
+            if email == email_helper:
+                Pedidos.objects.filter(id=id).update(terminado = True)
+                return Response(status=status.HTTP_200_OK)
+            else:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 
@@ -45,72 +50,66 @@ class DetailsListView(APIView):
 class MyClientListView(APIView):
 
     def post(self,request):
-        email_client = request.data.get('email_client', None)
-        queryset = Pedidos.objects.filter(email_client=email_client).values('helper_name','task_name','id')
-        return Response(data=queryset,status=status.HTTP_200_OK)
+        try:
+            email_client = request.data.get('email_client', None)
+            queryset = Pedidos.objects.filter(email_client=email_client).values()
+            return Response(data=queryset,status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class MyHelperListView(APIView):
 
     def post(self,request):
-        email_helper = request.data.get('email_helper', None)
-        queryset = Pedidos.objects.filter(email_helper=email_helper).values('client_name','task_name','id')
-        return Response(data=queryset,status=status.HTTP_200_OK)
-
+        try:
+            email_helper = request.data.get('email_helper', None)
+            queryset = Pedidos.objects.filter(email_helper=email_helper).values()
+            return Response(data=queryset,status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class AjudarListView(APIView):
 
     def post(self,request):
-        id = request.data.get('id',None)
-        helper_name = request.data.get('helper_name', None)
-        email_helper = request.data.get('email_helper', None)
-        helper_phone = request.data.get('helper_phone', None)
-        Pedidos.objects.filter(id=id).update(helper_name=helper_name,email_helper=email_helper,helper_phone=helper_phone,is_taken=True)
-        return Response(status=status.HTTP_200_OK)
-
+        try:
+            id = request.data.get('id',None)
+            helper_name = request.data.get('helper_name', None)
+            email_helper = request.data.get('email_helper', None)
+            helper_phone = request.data.get('helper_phone', None)
+            Pedidos.objects.filter(id=id).update(helper_name=helper_name,email_helper=email_helper,helper_phone=helper_phone,is_taken=True)
+            return Response(status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class PostarListView(generics.CreateAPIView): #APIView
     # def post(self,request):
-    #     description = request.data.get('description',None)
-    #    payment = request.data.get('payment', None)
-    #     pago = request.data.get('pago', None)
-    #     email_client = request.data.get('email_client', None)
-    #     email_helper = request.data.get('email_helper', None)
-    #     client_name = request.data.get('client_name', None)
-    #     client_phone = request.data.get('client_phone', None)
-    #     helper_name = request.data.get('helper_name', None)
-    #     helper_phone = request.data.get('helper_phone', None)
-    #     task_name = request.data.get('task_name', None)
 
         serializer_class = PedidosSerializer
 
         def create(self, request, *args, **kwargs):
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
+            try:
+                serializer = self.get_serializer(data=request.data)
+                serializer.is_valid(raise_exception=True)
+                self.perform_create(serializer)
+                headers = self.get_success_headers(serializer.data)
+                return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+            except:
+                return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 
 class AvailableListView(APIView):
     def get(self,request,format=None):
-        queryset=Pedidos.objects.filter(is_taken=False).values()
-        return Response(data=queryset,status=status.HTTP_200_OK)
-
+        try:
+            queryset=Pedidos.objects.filter(is_taken=False).values()
+            return Response(data=queryset,status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class LoginListView(APIView):
 
     permission_classes = (permissions.AllowAny,)
-    # queryset = User.objects.all()
-    # serializer_class = UserSerializer
-
-    # def retrieve(self, request, *args, **kwargs):
-    #     queryset = self.get_queryset().filter()
-    #     serializer = UserSerializer(queryset, many=False)
-    #     return Response(serializer.data)
 
     def get(self, request, format=None):
         return Response(status=status.HTTP_200_OK)
@@ -122,54 +121,21 @@ class LoginListView(APIView):
             qs = User.objects.filter(email=email, password=password).values()
             if not qs:
                 return Response(status=status.HTTP_401_UNAUTHORIZED)
-            #results = {'data': qs}
             return Response(data=qs, status=status.HTTP_200_OK)
-            #return Response(data=results, status=status.HTTP_200_OK)
         except:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class RegisterListView(generics.CreateAPIView):
     serializer_class = UserSerializer
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        try:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-
-
-# class UserListView(generics.ListAPIView):
-#     """
-#             get:
-#                 Search or get users
-#     """
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-
-# class ClientListView(generics.ListAPIView):
-#     """
-#             get:
-#                 Search or get users
-#     """
-#     queryset = Client.objects.all()
-#     serializer_class = ClientSerializer
-
-
-# class HelperListView(generics.ListAPIView):
-#     """
-#             get:
-#                 Search or get users
-#     """
-#     queryset = Helper.objects.all()
-#     serializer_class = HelperSerializer
-
-# class PedidosListView(generics.ListAPIView):
-#     """
-#             get:
-#                 Search or get users
-#     """
-#     queryset = Pedidos.objects.all()
-#     serializer_class = PedidosSerializer
 
